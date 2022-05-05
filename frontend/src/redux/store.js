208004ad60fe206b1b8f44470c3d3a72c7197f75
dufus <jake@schillingcentral.net>
2022-05-05 11:59:34 -0600

    Yeah

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,1 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+
+//import exampleReducer from './exampleReducer
+
+const store = configureStore({
+    reducer: {
+        //example: exampleReducer,
+    }
+})
