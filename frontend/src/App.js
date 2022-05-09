22474012dab9aff9cf04f930b711f00c8f4b15b8
dufus <jake@schillingcentral.net>
2022-05-09 09:39:24 -0600

    First example component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Router, Switch, Route, Link } from 'react-router-dom';
-import './features/auth/loginComponent'
+import LoginComponent from './features/auth/loginComponent';
 
 function App() {
 	return (
 		<div className='bg-indigo-500 text-white h-full w-full'>
 			<p className='text-5xl'>Hello World</p>
+			<LoginComponent></LoginComponent>
 		</div>
 	);
 }
--- frontend/src/features/auth/loginComponent.jsx
+++ frontend/src/features/auth/loginComponent.jsx
@@ -2,4 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
+const login = (props) => {
+	return (
+		<div>
+			<p className='text-white text-2xl'>I am in login</p>
+		</div>
+	);
+};
 
+export default login;
