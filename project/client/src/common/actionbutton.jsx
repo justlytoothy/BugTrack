1de63c6510baf56526fdc5ea2574b991f3173106
dufus <jake@schillingcentral.net>
2022-05-11 13:35:17 -0600

    fixed is active nav bar

--- project/client/src/common/actionbutton.jsx
+++ project/client/src/common/actionbutton.jsx
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import React from 'react';
 
 const ActionButton = (props) => {
 	let content = '';
--- project/client/src/features/sidebar/homeSidebarComponent.jsx
+++ project/client/src/features/sidebar/homeSidebarComponent.jsx
@@ -1,22 +1,33 @@
 import React, { useState } from 'react';
-import { Router, Link } from 'react-router-dom';
+import { useLocation, NavLink } from 'react-router-dom';
 import SidebarItems from './sidebarItems';
 
 const Sidebar = (props, { defaultActive }) => {
+	const location = useLocation().pathname;
 	return (
 		<>
-			<div className='bg-[#c34a36] w-64 h-[105vh]'>
+			<div className='bg-[#c34a36] w-64 h-[105vh] flex flex-col'>
 				{SidebarItems.map((item, index) => {
 					return (
-						<Link to={item.route} key={item.name}>
-							<div className='py-4 px-6 transition-all duration-100 ease-in-out my-1 mx-3 rounded hover:bg-gray-600 cursor-pointer'>
-								<p
-									className='text-white font-bold text-xl'
-									style={{ textDecoration: 'none' }}>
-									{item.name}
-								</p>
-							</div>
-						</Link>
+						<NavLink
+							to={item.route}
+							isActive={(match, location) => {
+								if (!match) {
+									return false;
+								}
+
+								// only consider an event active if its event id is an odd number
+								const eventID = parseInt(match.params.eventID);
+								return !isNaN(eventID) && eventID % 2 === 1;
+							}}
+							key={item.name}
+							className={({ isActive }) =>
+								isActive
+									? 'bg-gray-500 text-white font-bold text-xl py-4 px-6 transition-all duration-100 ease-in-out my-1 mx-3 rounded cursor-pointer'
+									: 'hover:bg-gray-500 text-white font-bold text-xl py-4 px-6 transition-all duration-100 ease-in-out my-1 mx-3 rounded cursor-pointer'
+							}>
+							{item.name}
+						</NavLink>
 					);
 				})}
 			</div>
@@ -25,3 +36,7 @@ const Sidebar = (props, { defaultActive }) => {
 };
 
 export default Sidebar;
+
+// 'bg-gray-500 text-white font-bold text-xl py-4 px-6 transition-all duration-100 ease-in-out my-1 mx-3 rounded cursor-pointer'
+// className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+// 'hover:bg-gray-500 text-white font-bold text-xl py-4 px-6 transition-all duration-100 ease-in-out my-1 mx-3 rounded cursor-pointer'
