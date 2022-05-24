a9b5018d40cf01d299f4035aac17c9475823d273
Jake Schilling <jake@schillingcentral.net>
2022-05-24 07:29:53 -0600

    fixed ticket employee display

--- project/client/src/features/projects/projectdetails/projectDetailsComponent.jsx
+++ project/client/src/features/projects/projectdetails/projectDetailsComponent.jsx
@@ -1,91 +1,90 @@
-import React, { useEffect, useState } from 'react';
-import { useOutletContext, useParams } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { getProject, getSelectedProject, refreshStatus } from '../projectSlice';
-import { refreshTicketStatus } from '../../tickets/ticketSlice';
-import { CSSTransition } from 'react-transition-group';
-import common from '../../../common/commonImports';
-import NewTicketComponent from '../../tickets/newTicketComponent';
-import Modal from 'react-modal';
+import React, { useEffect, useState } from 'react'
+import { useOutletContext, useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { getProject, getSelectedProject, refreshStatus } from '../projectSlice'
+import { refreshTicketStatus } from '../../tickets/ticketSlice'
+import { CSSTransition } from 'react-transition-group'
+import common from '../../../common/commonImports'
+import NewTicketComponent from '../../tickets/newTicketComponent'
+import Modal from 'react-modal'
 
-import TicketCard from '../../tickets/ticketCardComponent';
+import TicketCard from '../../tickets/ticketCardComponent'
 
 const ProjectDetails = (props) => {
-	const closeIt = useOutletContext();
-	const { id } = useParams();
-	const [selectedTicket, setSelectedTicket] = useState();
-	const [showDetails, setShowDetails] = useState(false);
-	const [modalIsOpen, setIsOpen] = useState(false);
-	const nodeRef = React.useRef(null);
-	const dispatch = useDispatch();
-	const refresh = useSelector(refreshStatus);
-	const refreshTicket = useSelector(refreshTicketStatus);
-	const project = useSelector(getSelectedProject);
+	const closeIt = useOutletContext()
+	const { id } = useParams()
+	const [selectedTicket, setSelectedTicket] = useState()
+	const [showDetails, setShowDetails] = useState(false)
+	const [modalIsOpen, setIsOpen] = useState(false)
+	const nodeRef = React.useRef(null)
+	const dispatch = useDispatch()
+	const refresh = useSelector(refreshStatus)
+	const refreshTicket = useSelector(refreshTicketStatus)
+	const project = useSelector(getSelectedProject)
 	useEffect(() => {
-		dispatch(getProject(id));
-	}, [refresh, refreshTicket]);
-	Modal.setAppElement('#root');
+		dispatch(getProject(id))
+	}, [refresh, refreshTicket])
+	Modal.setAppElement('#root')
 
 	const scrollMe = () => {
-		if (!nodeRef) return;
+		if (!nodeRef) return
 		// Get node coords from Ref
-		const node =
-			nodeRef.current.getBoundingClientRect().top + window.scrollY;
+		const node = nodeRef.current.getBoundingClientRect().top + window.scrollY
 
 		window.scroll({
 			top: node,
 			behavior: 'smooth',
-		});
-	};
+		})
+	}
 	const scrollMeFirst = () => {
-		nodeRef.current.scrollIntoView(true);
-	};
+		nodeRef.current.scrollIntoView(true)
+	}
 	const toTop = () => {
 		window.scroll({
 			top: 0,
 			left: 0,
 			behavior: 'smooth',
-		});
-	};
+		})
+	}
 
 	const openForm = () => {
-		setIsOpen(true);
-	};
+		setIsOpen(true)
+	}
 	const closeForm = () => {
-		setIsOpen(false);
-	};
+		setIsOpen(false)
+	}
 
 	const openDetails = () => {
-		setShowDetails(true);
-	};
+		setShowDetails(true)
+	}
 	const closeDetails = () => {
-		setShowDetails(false);
-	};
+		setShowDetails(false)
+	}
 	const closeDetailsScroll = () => {
-		setShowDetails(false);
-		setTimeout(() => toTop(), 100);
-	};
+		setShowDetails(false)
+		setTimeout(() => toTop(), 100)
+	}
 
 	const showTicket = (ticket) => {
 		if (ticket !== selectedTicket && showDetails === true) {
-			scrollMe();
+			scrollMe()
 			setTimeout(() => {
-				closeDetails();
+				closeDetails()
 				setTimeout(() => {
-					setSelectedTicket(ticket);
-					openDetails();
-				}, 500);
-			}, 250);
+					setSelectedTicket(ticket)
+					openDetails()
+				}, 500)
+			}, 250)
 		} else {
-			setSelectedTicket(ticket);
+			setSelectedTicket(ticket)
 			if (showDetails === true) {
-				closeDetails();
+				closeDetails()
 			} else {
-				setTimeout(() => scrollMeFirst(), 1);
-				openDetails();
+				setTimeout(() => scrollMeFirst(), 1)
+				openDetails()
 			}
 		}
-	};
+	}
 
 	const listTickets = () => {
 		return (
@@ -109,9 +108,9 @@ const ProjectDetails = (props) => {
 				</div>
 				{React.Children.toArray(
 					project.tickets.map((ticket) => {
-						let iter = project.tickets.length - 1;
+						let iter = project.tickets.length - 1
 						if (iter !== 0) {
-							iter--;
+							iter--
 							return (
 								<div
 									className='grid grid-cols-8 hover:bg-white-filled focus:bg-white-filled cursor-pointer active:bg-rich-black active:text-white focus:bg-rich-black focus:text-white'
@@ -127,13 +126,13 @@ const ProjectDetails = (props) => {
 										{ticket.ticket_type}
 									</span>
 									<span className='p-2 border-r border-b border-rich-black col-span-2'>
-										{ticket.ticket_creator}
+										{`${ticket.ticket_creator.first_name} ${ticket.ticket_creator.last_name}`}
 									</span>
 									<span className='p-2 border-b border-rich-black col-span-2'>
-										{ticket.ticket_description}
+										{`${ticket.assigned_employees[0].first_name} ${ticket.assigned_employees[0].last_name}`}
 									</span>
 								</div>
-							);
+							)
 						} else {
 							return (
 								<div
@@ -156,13 +155,13 @@ const ProjectDetails = (props) => {
 										{`${ticket.assigned_employees[0].first_name} ${ticket.assigned_employees[0].last_name}`}
 									</span>
 								</div>
-							);
+							)
 						}
 					})
 				)}
 			</div>
-		);
-	};
+		)
+	}
 
 	const listEmployees = () => {
 		return (
@@ -180,17 +179,15 @@ const ProjectDetails = (props) => {
 				</div>
 				{React.Children.toArray(
 					project.employees.map((employee) => {
-						let iter = project.employees.length - 1;
+						let iter = project.employees.length - 1
 						if (iter !== 0) {
-							iter--;
+							iter--
 							return (
 								<div
 									className='grid grid-cols-4 hover:bg-white-filled focus:bg-white-filled cursor-pointer'
 									tabIndex={project.employees.length - iter}>
 									<span className='p-2 border-r border-b border-rich-black col-span-2'>
-										{employee.first_name +
-											' ' +
-											employee.last_name}
+										{employee.first_name + ' ' + employee.last_name}
 									</span>
 									<span className='p-2 border-r border-b border-rich-black col-span-1'>
 										{employee.role}
@@ -199,16 +196,14 @@ const ProjectDetails = (props) => {
 										{employee.role}
 									</span>
 								</div>
-							);
+							)
 						} else {
 							return (
 								<div
 									tabIndex={project.employees.length - iter}
 									className='grid grid-cols-4 hover:bg-white-filled focus:bg-white-filled cursor-pointer'>
 									<span className='p-2 border-r border-b border-rich-black col-span-2'>
-										{employee.first_name +
-											' ' +
-											employee.last_name}
+										{employee.first_name + ' ' + employee.last_name}
 									</span>
 									<span className='p-2 border-r border-b border-rich-black col-span-1'>
 										{employee.role}
@@ -217,13 +212,13 @@ const ProjectDetails = (props) => {
 										{employee.role}
 									</span>
 								</div>
-							);
+							)
 						}
 					})
 				)}
 			</div>
-		);
-	};
+		)
+	}
 
 	if (Object.keys(project).length > 0) {
 		return (
@@ -292,7 +287,8 @@ const ProjectDetails = (props) => {
 						}}
 						unmountOnExit
 						classNames={{
-							enter: 'scale-y-0 duration-300 transition-all motion-reduce:transition-none transform origin-center',
+							enter:
+								'scale-y-0 duration-300 transition-all motion-reduce:transition-none transform origin-center',
 							enterActive:
 								'scale-y-100 duration-300 transition-all motion-reduce:transition-none transform origin-center',
 							enterDone:
@@ -325,10 +321,10 @@ const ProjectDetails = (props) => {
 					</Modal>
 				</div>
 			</div>
-		);
+		)
 	} else {
-		return <div>Loading Project Info...</div>;
+		return <div>Loading Project Info...</div>
 	}
-};
+}
 
-export default ProjectDetails;
+export default ProjectDetails
